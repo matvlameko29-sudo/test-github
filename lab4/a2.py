c80f199f0d32d7268af976948f4354281a71e720
Matvey Lameko <matv.lameko29@gmail.com>
2025-11-18 10:23:10 +0300

    Update a2.py

--- lab4/a2.py
+++ lab4/a2.py
@@ -1,32 +1,17 @@
-
-def rect(rows, cols, ch):
-    for i in range(rows):
-        for j in range(cols):
-            print(ch, end="")
-        print()
-
-def triangle(n, ch):
-    for i in range(1, n + 1):
-        print(ch * i)
-
-def frame(rows, cols, ch):
-    for i in range(rows):
-        for j in range(cols):
-            if i == 0 or i == rows-1 or j == 0 or j == cols-1:
-                print(ch, end="")
-            else:
-                print(" ", end="")
-        print()
-
-n = int(input("Строки: "))
-m = int(input("Столбцы: "))
-ch = input("Символ: ") or "#"
-
-print("\nПрямоугольник:")
-rect(n, m, ch)
-
-print("\nТреугольник:")
-triangle(n, ch)
-
-print("\nРамка:")
-frame(n, m, ch)
+x = int(input("x = "))
+y = int(input("y = "))
+i = 1
+n = 1
+m = 1
+while i <= y:
+    print("#"*x)
+    i+=1
+while n <= y:
+    print("#"*n)
+    n+=1
+while m <= y:
+    if m==1 or m==y:
+        print("#"*x)
+    if m>=1 and m<=y-1:
+        print("#", " "*(x-4), "#")
+    m+=1
